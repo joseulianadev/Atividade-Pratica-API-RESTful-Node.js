ee67ab995a712e5a6d2e8d8a6ba8a15caf6aafe9
Jose Uliana <ulianajosee@gmail.com>
2026-08-30 15:48:46 -0300

    Garante que o servidor escuta em todas as interfaces

--- server.js
+++ server.js
@@ -155,6 +155,6 @@ app.delete('/produtos/:id', (req, res) => {
 })
 
 const port = process.env.PORT || 3000
-app.listen (port, ()=>{
+app.listen (port, '0.0.0.0', ()=>{
     console.log (`Server rodando no http://localhost:${port}`)
 })
